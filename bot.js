17b69ff39b00b6fef16e0662ef2b9745e9f8019f
hakika100 <56877986+hakika100@users.noreply.github.com>
2019-10-27 10:00:14 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -57,7 +57,7 @@ client.on('ready',  () => {
     if(!message.channel.guild) return;
 let args = message.content.split(' ').slice(1).join(' ');
 if (message.content.startsWith('$adminbc')){
-if(!message.author.id === '446029696212271162') return;
+if(!message.author.id === '635128366516928513') return;
 message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
 client.users.forEach(m =>{
 m.sendMessage(args)
@@ -90,7 +90,7 @@ m.sendMessage(args)
    }
    });
 
-const developers = ["446029696212271162","id"]
+const developers = ["635128366516928513","id"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
